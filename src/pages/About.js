e4872064faaea25e503a29bd603c7b967a5c126c
frederic <dewaele-frederic@outlook.fr>
2022-09-18 17:38:20 +0200

    flag finish

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>à propos</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo veniam ut animi voluptatem beatae, molestias exercitationem fuga placeat asperiores illum quia quos ex, vitae officia quaerat maxime eos! Explicabo, voluptatibus!</p>
+        </div>
+    );
+};
+
+export default About;
